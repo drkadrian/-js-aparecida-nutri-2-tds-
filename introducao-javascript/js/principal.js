cb977c03999349046512de941120d0898d6a5281
drk.adrian <164528138+drkadrian@users.noreply.github.com>
2024-09-20 12:29:45 -0300

    Add files via upload

---
+++ introducao-javascript/js/principal.js
@@ -0,0 +1,6 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente= document.queryselector("#primeiro-paciente");
+
+console. log(paciente);
